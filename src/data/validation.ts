28192540cc6658096d4a3d7e3267d1de090754a1
g0657 <g065700@naver.com>
2025-02-24 01:56:30 +0900

    #39: 박스오피스 데이터 캐시 이슈 해결

--- src/data/validation.ts
+++ src/data/validation.ts
@@ -1,12 +1,12 @@
 export function getSecondsUntilMidnight() {
   const now = new Date();
   const nextUpdate = new Date();
-  // nextUpdate.setHours(0, 5, 0, 0);
-  nextUpdate.setHours(1, 47, 0, 0);
+  // nextUpdate.setUTCHours(15, 5, 0, 0) // 00:05 KST = UTC 기준 15:05
+  nextUpdate.setUTCHours(17, 5, 0, 0);
 
-  // 현재 시간이 00:05 이후라면, 다음날 00:05로 설정
+  // 현재 시간이 00:05(KST) 이후라면, 다음날 00:05(KST)로 설정
   if (now > nextUpdate) {
-    nextUpdate.setDate(nextUpdate.getDate() + 1);
+    nextUpdate.setUTCDate(nextUpdate.getUTCDate() + 1);
   }
 
   console.log(
@@ -21,19 +21,19 @@ export function getSecondsUntilMondayMidnight() {
   const now = new Date();
   const nextUpdate = new Date();
 
-  const dayOfWeek = now.getDay();
+  const dayOfWeek = now.getUTCDay();
 
   // 월요일로 이동하기 위해 필요한 날짜 차이 계산
   const daysUntilMonday = (1 - dayOfWeek + 7) % 7; // 1이 월요일 (0은 일요일)
 
-  // 월요일 00:05로 설정
-  nextUpdate.setDate(now.getDate() + daysUntilMonday);
-  // nextUpdate.setHours(0, 5, 0, 0); // 00:05
-  nextUpdate.setHours(1, 47, 0, 0);
+  // 월요일 00:05 KST로 설정 (UTC 15:05)
+  nextUpdate.setUTCDate(now.getUTCDate() + daysUntilMonday);
+  // nextUpdate.setUTCHours(15, 5, 0, 0); // 15:05 UTC, 즉 00:05 KST
+  nextUpdate.setUTCHours(17, 5, 0, 0);
 
   // 현재 시간이 월요일 00:05 이후라면, 다음 주 월요일 00:05로 설정
   if (now > nextUpdate) {
-    nextUpdate.setDate(nextUpdate.getDate() + 7); // 7일 더해서 다음 주 월요일로 설정
+    nextUpdate.setUTCDate(nextUpdate.getUTCDate() + 7); // 7일 더해서 다음 주 월요일로 설정
   }
 
   console.log(
