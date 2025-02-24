37538f647e0107f74901f2f4c75b28f2ea9b20a3
g0657 <g065700@naver.com>
2025-02-24 21:02:33 +0900

    #42: revalidateTag 를 이용한 캐시 무효화 처리

--- src/app/actions/box-office/getDailyBoxOffice.ts
+++ src/app/actions/box-office/getDailyBoxOffice.ts
@@ -1,28 +1,51 @@
 'use server';
 
+import { revalidateTag } from 'next/cache';
 import { KobisDailyBoxOfficeRes } from '@/types/box-office/box-office';
 import { getYesterday } from '@/helpers/getDate';
 import { getSecondsUntilMidnight } from '@/data/validation';
 
-export default async function getDailyBoxOffice() {
-  const yesterday = getYesterday();
+const yesterday = getYesterday();
+const url = `https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${process.env.KOBIS_KEY}&targetDt=${yesterday}`;
 
+export default async function getDailyBoxOffice() {
   try {
-    const res = await fetch(
-      `https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${process.env.KOBIS_KEY}&targetDt=${yesterday}`,
-      {
-        method: 'GET',
-        next: {
-          revalidate: getSecondsUntilMidnight(),
-        },
+    const res = await fetch(url, {
+      method: 'GET',
+      cache: 'force-cache',
+      next: {
+        tags: ['daily-box-office'],
+        revalidate: getSecondsUntilMidnight(),
       },
-    );
+    });
 
     if (!res.ok) {
       throw new Error(`HTTP error! Status: ${res.status}`);
     }
 
-    const data: KobisDailyBoxOfficeRes = await res.json();
+    let data: KobisDailyBoxOfficeRes = await res.json();
+
+    const isLatestData =
+      data.boxOfficeResult.showRange.split('~')[0] === yesterday;
+
+    const now = new Date();
+    const isRevalidateTime =
+      (now.getUTCHours() >= 15 && now.getUTCMinutes() >= 5) ||
+      now.getUTCHours() > 15;
+
+    if (!isLatestData && isRevalidateTime) {
+      console.log('캐시 무효화 후 데이터 다시 가져오기...(일별)');
+      revalidateTag('daily-box-office'); // 캐시 삭제
+      const newRes = await fetch(url, {
+        cache: 'no-cache', // 강제로 새 데이터를 가져옴
+      });
+
+      if (!newRes.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+      }
+
+      data = await newRes.json();
+    }
 
     return {
       data,
